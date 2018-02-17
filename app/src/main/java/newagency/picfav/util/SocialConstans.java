6a987cca284a78e8f425d4357553169f570e7e90
Vova Tkach <vovatkach2427@gmail.com>
2018-02-17 15:59:26 +0200

    refactor share social code

---
+++ app/src/main/java/newagency/picfav/util/SocialConstans.java
@@ -0,0 +1,8 @@
+package newagency.picfav.util;
+
+public class SocialConstans {
+
+    public final static String MARKET_URL = "market://details?id=";
+    public final static String INSTAGRAM_PACKAGE = "com.instagram.android";
+    public final static String FACEBOOK_PACKAGE = "com.facebook.katana";
+}
